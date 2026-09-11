2aa3b6146fb7c5784dc418677002fe19c7ce7346
vanshnt <Vanshbijwani@gmail.com>
2026-09-11 11:01:03 +0530

    1.0.0

---
+++ grading.py
@@ -0,0 +1,15 @@
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Grade O")
+elif marks >= 80:
+    print("Grade A")
+elif marks >= 60:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+elif marks <= 45:
+    print("You failed!")
+
+
+    
